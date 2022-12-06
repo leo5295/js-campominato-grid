9b670e142ad23e2a743b373ba3a47a4ac9f5c791
leo5295 <leonardo.martellosio@gmail.com>
2022-12-06 17:43:44 +0100

    numbers, click

--- js/script.js
+++ js/script.js
@@ -1,21 +1,54 @@
-function createGridSquare() {
+function createGridSquare(number) {
 
     const currentElement = document.createElement('div');
 
     currentElement.classList.add('square');
 
+    if(number % 2 == 0){
+        currentElement.classList.add('even')
+    }
+    else{
+        currentElement.classList.add('odd')
+    }
+
+    currentElement.innerText = number;
+
     return currentElement;
     
 }
 
+function randomInteger(min,max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function uniqueRandomNumber(numBlackList, min, max){
+
+    let isValid = false;
+    let randomNum;
+
+    while(isValid == false){
+        randomNum = randomInteger(min, max);
+
+        if(numBlackList.includes(randomNum) == false){
+            isValid = true
+        }
+    }
+    return randomNum;
+}
+
+const blackList = [];
 let grid = document.getElementById('grid')
 
 for(let i=0; i<100; i++){
 
-    const currentSquare = createGridSquare();
+    let randomNumber = uniqueRandomNumber(blackList, 1,100);
+    console.log(randomNumber)
 
+    blackList.push(randomNumber);
+
+    const currentSquare = createGridSquare(randomNumber);
     currentSquare.addEventListener('click', function(){
-        this.classList.add('clicked')
+        this.classList.toggle('clicked')
     })
 
     grid.appendChild(currentSquare);
